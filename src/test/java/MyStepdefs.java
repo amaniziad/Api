e4dba70189c5cf599c95bc5121f5254a56a59868
amaniziad <amaniziad66@gmail.com>
2026-02-10 11:17:31 +0100

    api

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -2,6 +2,7 @@ import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class MyStepdefs {
 
